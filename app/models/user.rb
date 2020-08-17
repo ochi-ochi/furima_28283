d50df8770b016a5c9ef0d97913904969426c757c
ochi-ochi <pgame.pgame@gmail.com>
2020-08-17 15:26:48 +0900

    devese add:newuser

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ with_options presence: true do 
+  validates :name
+  validates :birth_date
+  validates :email,    uniqueness: {case_sensitive: false},
+                       format: {with: /\A[\w+\-.]+@[a-z\d\-.]/}
+  validates :encrypted_password, length: {minimum: 6}
+
+  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+end
+
 end
