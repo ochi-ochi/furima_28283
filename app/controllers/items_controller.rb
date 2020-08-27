60c07873561d5e629ffb9846a6be2e9367ad15da
ochi-ochi <pgame.pgame@gmail.com>
2020-08-27 19:37:35 +0900

    編集　imporve:action change:action-create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,17 +11,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    if Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
     end
-  end
-
-  def show
-  end
-
-  def edit 
   end
 
   def update
