ea3a7e35ee8a3f5c25f782160c03eb9ab34dd0ad
ochi-ochi <pgame.pgame@gmail.com>
2020-08-27 19:03:30 +0900

    編集機能　improve:action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.all.order(id: :DESC)
@@ -18,11 +19,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit 
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -44,5 +43,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
