eaacbcd4110765a6b69a7db5ba20da9decd43afc
ochi-ochi <pgame.pgame@gmail.com>
2020-08-28 19:37:06 +0900

    購入機能　improve:destroy-action add:f.object

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,16 @@
 class PurchasesController < ApplicationController 
   
   def index
+    @item = Item.find(params[:id])
   end
 
   def create
+    @purchase = PurchaseAdd.new(purchase_params)
+    @purchase.save
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_add).permit(:user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_number, :purchase)
   end
 end
