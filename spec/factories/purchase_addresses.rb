7eef0b034e0b9d41e2cc351c66420b7f0027db90
ochi-ochi <pgame.pgame@gmail.com>
2020-09-01 00:11:25 +0900

    購入機能　modeltest

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,12 +1,13 @@
-# FactoryBot.define do
-#   factory :purchase_address do
-#     create_table :purchase_adds do |t|
-#       postal_code       {'123-4567'}
-#       prefecture_id     {'2'}
-#       city              {'君津市'}
-#       house_number      {'1-1'}
-#       building_name     {'ハイツ'}
-#       tel_number        {'09012345678'}
-#     end
-#   end
-# end
+ FactoryBot.define do
+   factory :purchase_address do
+       postal_code       {'123-4567'}
+       prefecture_id     {'2'}
+       city              {'君津市'}
+       house_number      {'1-1'}
+       building_name     {'ハイツ'}
+       tel_number        {'09012345678'}
+       user_id           {'11'}
+       item_id           {'123'}
+       token             {'aqwsdefrgtyhujik'}
+     end
+   end
