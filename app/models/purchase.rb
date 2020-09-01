a12b750f5e77677049f4c7c01bcc25421095614d
ochi-ochi <pgame.pgame@gmail.com>
2020-09-01 16:24:48 +0900

    購入機能　delete:purchase validation

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,9 +2,4 @@ class Purchase < ApplicationRecord
   belongs_to :item
   belongs_to :user
   has_one :purchase_add
-
-  # with_options presence: true do
-  #   validates :user
-  #   validates :items
-  # end
 end
